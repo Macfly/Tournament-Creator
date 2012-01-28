130412d16349965ad2db240ead1c3d6ed27658b2
Macfly <cocoval@gmail.com>
2012-01-28 16:14:30 +0100

    second commit

---
+++ src/com/tournament/creator/TournamentCreatorActivity.java
@@ -0,0 +1,13 @@
+package com.tournament.creator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TournamentCreatorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
